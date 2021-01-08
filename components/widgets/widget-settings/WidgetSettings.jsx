8953b76c138dc00d5185edf4646f9c8a12949171
Eugen Volosciuc <volosciuc.eugen@gmail.com>
2021-01-08 21:03:35 +0200

    Changed display of widget settings

--- components/widgets/widget-settings/WidgetSettings.jsx
+++ components/widgets/widget-settings/WidgetSettings.jsx
@@ -4,14 +4,20 @@ import styles from 'components/widgets/styles/WidgetPositioner.module.scss'
 import WidgetRemoveSetting from 'components/widgets/widget-settings/WidgetRemoveSetting'
 import WidgetMoveSetting from 'components/widgets/widget-settings/WidgetMoveSetting'
 // import WidgetResizeSetting from 'components/widgets/WidgetSettings/WidgetResizeSetting'
+import useDashboardEditMode from 'utils/hooks/useDashboardEditMode'
 
 const WidgetSettings = ({ widget, setWidgetIsMoving }) => {
+  const { dashboardEditMode } = useDashboardEditMode()
+  
   return (
-    <div className={styles['widget-settings']}>
-      <WidgetMoveSetting widget={widget} setWidgetIsMoving={setWidgetIsMoving} />
-      {/* <WidgetResizeSetting widget={widget} /> */}
-      <WidgetRemoveSetting widget={widget} />
-    </div>
+    <>
+      <div className={styles['left-top']}>
+        <WidgetMoveSetting widget={widget} setWidgetIsMoving={setWidgetIsMoving} />
+      </div>
+      <div className={`${styles['right-top']} ${dashboardEditMode ? 'hidden' : ''}`}>
+        <WidgetRemoveSetting widget={widget} />
+      </div>
+    </>
   )
 }
 
